2f2708bdbba3f3953819d5153a2868b7ae24c1b8
Pepo Banchev <banchevpepo@gmail.com>
2023-11-16 20:17:18 +0200

    up

--- weather-app/src/Components/WeatherApp/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp/WeatherApp.jsx
@@ -10,11 +10,32 @@ import rain_icon from "../assets/rain.png"
 import wind_icon from "../assets/wind.png"
 
 const WeatherApp = () => {
+    let api_key = "82b6d2555fa4a0faf3240d81e35cb76b";
+
+    const search = async () => {
+        const element = document.getElementsByClassName('cityInput');
+        if (element[0].value === "") {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${elemet[0].value}&appid=${api_key}`;
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity = document.getElementsByClassName("humidity-percent");
+        const wind = document.getElementsByClassName("wind-rate");
+        const temperature = document.getElementsByClassName('weather-temp');
+        const location = document.getElementsByClassName('weather-location');
+
+        humidity[0].innerHTML = data.main.humidity;
+        wind[0].innerHTML = data.wind.speed;
+        temperature[0].innerHTML = data.main.temp;
+        location[0].innerHTML = data.name;
+
+    }
     return (
         <div className='container'>
             <div className="top-bar"></div>
             <input type="text" className="cityInput" placeholder='search' />
-            <div className="search-icon">
+            <div className="search-icon" onClick={() => { search() }}>
                 <img src={search_icon} alt="" />
             </div>
             <div className="weather-image">
@@ -33,7 +54,7 @@ const WeatherApp = () => {
             <div className="element">
                 <img src={wind_icon} alt='' className='icon' />
                 <div className='data'>
-                    <div className='humidity-percent'>18 km/h</div>
+                    <div className='wind-rate'>18 km/h</div>
                     <div className='text'>Wind Speed</div>
                 </div>
             </div>
