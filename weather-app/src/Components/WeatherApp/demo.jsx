de10f39d3be0b4f4541c1228e753934ce1201e1c
Pepo Banchev <banchevpepo@gmail.com>
2023-11-20 00:40:14 +0200

    update
    changed ui

--- weather-app/src/Components/WeatherApp/demo.jsx
+++ weather-app/src/Components/WeatherApp/demo.jsx
@@ -1,43 +1,41 @@
-import "../WeatherApp/WeatherApp.css";
-import { useState } from "react";
+import React, { useState } from 'react';
+import './WeatherApp.css';
+import search_icon from '../assets/search.png';
 import clear_icon from '../assets/clear.png';
 import cloud_icon from '../assets/cloud.png';
-import drizzle_icon from "../assets/drizzle.png"
-import rain_icon from "../assets/rain.png";
-import snow_icon from "../assets/snow.png";
-import humidity_icon from "../assets/humidity.png";
-import search_icon from "../assets/search.png";
-import wind_icon from "../assets/wind.png";
-
-
-import React from 'react'
+import drizzle_icon from '../assets/drizzle.png';
+import humidity_icon from '../assets/humidity.png';
+import snow_icon from '../assets/snow.png';
+import rain_icon from '../assets/rain.png';
+import wind_icon from '../assets/wind.png';
 
 const WeatherApp = () => {
     let api_key = '82b6d2555fa4a0faf3240d81e35cb76b';
     const [icon, setIcon] = useState(clear_icon);
-    const [location, setLocation] = useState('');
     const [humidity, setHumidity] = useState('');
-    const [wind, setWind] = useState('');
+    const [windSpeed, setWindSpeed] = useState('');
     const [temperature, setTemperature] = useState('');
+    const [location, setLocation] = useState('');
 
     const fetchData = async (city) => {
         try {
             if (!city) {
                 return;
             }
+
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${api_key}`;
             let response = await fetch(url);
             let data = await response.json();
 
             setHumidity(data.main.humidity);
-            setWind(Math.floor(data.wind.speed));
-            setLocation(data.name);
+            setWindSpeed(Math.floor(data.wind.speed));
             setTemperature(Math.floor(data.main.temp));
+            setLocation(data.name);
             updateWeatherIcon(data.weather[0].icon);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-    }
+    };
 
     const updateWeatherIcon = (weatherIcon) => {
         const weatherIcons = {
@@ -56,9 +54,10 @@ const WeatherApp = () => {
             '13d': snow_icon,
             '13n': snow_icon,
         };
+
         const selectedIcon = weatherIcons[weatherIcon] || clear_icon;
         setIcon(selectedIcon);
-    }
+    };
 
     const search = () => {
         const element = document.getElementsByClassName('cityInput');
@@ -94,13 +93,13 @@ const WeatherApp = () => {
                 <div className="element">
                     <img src={wind_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="wind-rate">{wind} km/h</div>
+                        <div className="wind-rate">{windSpeed} km/h</div>
                         <div className="text">Wind Speed</div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
-export default WeatherApp
+export default WeatherApp;
